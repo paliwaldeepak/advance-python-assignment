6b754b0c578c0b9401b051db67106bf75b4cab2c
DEEPAK PALIWAL <114053130+paliwaldeepak@users.noreply.github.com>
2023-05-08 13:30:39 +0530

    Add files via upload

---
+++ python advance assignment 1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1. What is the purpose of Python's OOP?
+
+# #ans:
+# It allows us to develop applications using an Object-Oriented approach. In Python, we can easily create and use classes and objects. An object-oriented paradigm is to design the program using classes and objects.
+
+# Q2. Where does an inheritance search look for an attribute?
+
+# #ans:-
+# An inheritance search looks for an attribute first in the instance object, then in the class the instance was created from, then in all higher superclasses, progressing from left to right (by default)
+
+# Q3. How do you distinguish between a class object and an instance object?
+
+# #ans:-
+# while the terms 'object' and 'instance' are interchangeable, the term 'instance' refers to an object's relationship to its class.
+
+# Q4. What makes the first argument in a class’s method function special?
+
+# ans:-
+#     The calling process is automatic while the receiving process is not . This is the reason the first parameter of a function in class must be the object itself
+
+# Q5. What is the purpose of the __init__ method?
+
+# ans:-
+#     This method is called when an object is created from a class and it allows the class to initialize the attributes of the class
+
+# Q6. What is the process for creating a class instance?
+
+# In[ ]:
+
+
+ans:-
+    To create instances of a class, you call the class using class name and pass in whatever arguments its __init__ method accepts.
+
+
+# Q7. What is the process for creating a class?
+
+# ans:-
+#     a class can be created by using the keyword class, followed by the class name
+
+# Q8. How would you define the superclasses of a class?
+
+# ans:-
+#     A class that is derived from another class is called a subclass (also a derived class, extended class, or child class). The class from which the subclass is derived is called a superclass (also a base class or a parent class).
