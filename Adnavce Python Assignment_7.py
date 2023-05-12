814d3bdef0ff368cec191fa748136d21f693ede7
DEEPAK PALIWAL <114053130+paliwaldeepak@users.noreply.github.com>
2023-05-12 15:26:37 +0530

    Add files via upload

---
+++ Adnavce Python Assignment_7.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1. What is the purpose of the try statement?
+# #ans:-
+# The try statement allows you to define a block of code to be tested for errors while it is being executed. 
+# 
+# Q2. What are the two most popular try statement variations?
+# #ans:-
+# Two most popular try statement variations are try except and try except finally.
+# 
+# Q3. What is the purpose of the raise statement?
+# #ans:-
+# The raise keyword raises a specific exception when a condition is met or the code encounters an error. The exception raised by the program may either be an exception instance or an exception class. When you use the raise keyword, you can define what kind of error the machine should raise for a particular exception.
+# 
+# Q4. What does the assert statement do, and what other statement is it like?
+# #ans:-
+# The assert statement is used to continue the execute if the given condition evaluates to True. If the assert condition evaluates to False, then it raises the Assertion Error exception with the specified error message. Assertion Error is also a built-in exception that can be handled using try-except construct
+# 
+# Q5. What is the purpose of the with/as argument, and what other statement is it like?
+# #ans:-
+# The with statement in Python replaces a try-catch block with a simple shorthand. More significantly, it ensures that resources are closed immediately after processing. Reading or writing to a file is a common use of the with statement. A context manager is a function or class that supports the with statement.
+# 
+# 
