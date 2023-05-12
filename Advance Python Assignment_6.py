4f87d5f9e1e9c9a64b08467f926b6e4b8d04d1cc
DEEPAK PALIWAL <114053130+paliwaldeepak@users.noreply.github.com>
2023-05-12 12:50:40 +0530

    Add files via upload

---
+++ Advance Python Assignment_6.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1. Describe three applications for exception processing.
+# #ans:-
+# Exception handling is the process of responding to unwanted or unexpected events when a computer program runs. Exception handling deals with these events to avoid the program or system crashing, and without this process, exceptions would disrupt the normal operation of a program.
+# 
+# 
+# Q2. What happens if you don't do something extra to treat an exception?
+# #ans:-
+# When an exception occurred, if you don't handle it, the program terminates abruptly and the code past the line that caused the exception will not get executed
+# 
+# 
+# Q3. What are your options for recovering from an exception in your script?
+# #ans:-
+# You can use the catchError() script operation to catch exceptions in the scripting code, and take the required action that is based on the exception. You can choose to handle the exception programmatically or rethrow the exception to calling code.
+# 
+# 
+# Q4. Describe two methods for triggering exceptions in your script.
+# #ans:-
+# To avoid such a scenario, there are two methods to handle Python exceptions: Try – This method catches the exceptions raised by the program. Raise – Triggers an exception manually using custom exceptions.
+# 
+# 
+# Q5. Identify two methods for specifying actions to be executed at termination time, regardless of whether or not an exception exists.
+# #ans:-
+# If no exception occurs during the execution, the execution will reach the break statement and the while loop will be left. If an exception occurs, i.e. in the casting of n, the rest of the try block will be skipped and the except clause will be executed.
+# 
