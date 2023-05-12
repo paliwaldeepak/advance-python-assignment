0c49e00d175b3c029cb18896f14c534c5db85763
DEEPAK PALIWAL <114053130+paliwaldeepak@users.noreply.github.com>
2023-05-12 11:47:19 +0530

    Add files via upload

---
+++ Advance Python Assignment 5.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q1. What is the meaning of multiple inheritance?
+# #ans:
+# If a child class is inheriting the properties of a single other class, we call it single inheritance. However, if a child class inherits from more than one class, i.e. this child class is derived from multiple classes, we call it multiple inheritance in Python.
+# 
+# 
+# Q2. What is the concept of delegation?
+# #ans:-
+# Delegation is the assignment of authority to another person (normally from a manager to a subordinate) to carry out specific activities. It is the process of distributing and entrusting work to another person, and therefore one of the core concepts of management leadership.
+# 
+# 
+# Q3. What is the concept of composition?
+# #ans:-
+# Composition is one of the fundamental concepts in object-oriented programming. It describes a class that references one or more objects of other classes in instance variables. This allows you to model a has-a association between objects.
+# 
+# 
+# Q4. What are bound methods and how do we use them?
+# #ans:-
+# The Bound methods are the methods defined in a class. The bound methods are bound to the class in which they are defined. They take an instance of their class along with the other arguments as its parameters. Bound methods differ from the other unbound methods in their first parameter.
+# 
+# 
+# Q5. What is the purpose of pseudoprivate attributes?
+# #ans:-
+# On the other hand, instance attributes, which are defined in the __init__() function, are class variables that allow us to define different values for each object of a class
+# 
