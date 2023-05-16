48ace66603c57beefaece474c8c7b63a8b7b132b
DEEPAK PALIWAL <114053130+paliwaldeepak@users.noreply.github.com>
2023-05-16 12:18:50 +0530

    Add files via upload

---
+++ Advance Python Assignment_15.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1.	What are the new features added in Python 3.8 version?
+# #ans:-
+# Assignment Expressions.
+# Positional-Only Parameters.
+# Parallel Filesystem Cache For Compiled Bytecode Files.
+# Debug Build Uses The Same ABI as Release Build.
+# f-Strings Support = For Self Documenting Expressions And Debugging.
+# Python Runtime Audit Hooks.
+# Python Initialization Configuration.
+# 
+# 
+# 2.	What is monkey patching in Python?
+# #ans:-
+# Monkey patching in python refers to modifying or updating a piece of code or class or any module at the runtime. In simple words, we can change the behavior or working of a class/ module at the runtime without changing the whole python code.
+# 
+# 3.	What is the difference between a shallow copy and deep copy?
+# #ans:-
+# A shallow copy constructs a new compound object and then (to the extent possible) inserts references into it to the objects found in the original. A deep copy constructs a new compound object and then, recursively, inserts copies into it of the objects found in the original.
+# 
+# 4.	What is the maximum possible length of an identifier?
+# #ans:-
+# An identifier can have a maximum length of 79 characters in Python.
+# 
+# 5.	What is generator comprehension?
+# #ans:-
+# A generator comprehension is a single-line specification for defining a generator in Python. It is absolutely essential to learn this syntax in order to write simple and readable code. Note: Generator comprehensions are not the only method for defining generators in Python.
+# 
